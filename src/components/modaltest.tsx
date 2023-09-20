6affa91a9bc59859b564703268186f7bc4e3a688
MichaelMaherKamel <michaelmaherkamel@gmail.com>
2023-09-20 16:20:08 +0300

    Sign-In Modal #1

--- src/components/modaltest.tsx
+++ src/components/modaltest.tsx
@@ -27,6 +27,7 @@ export default function ModalTest() {
         backdrop='blur'
         isOpen={isOpen}
         onOpenChange={onOpenChange}
+        placement='center'
         classNames={{
           backdrop: 'bg-gradient-to-t from-zinc-900 to-zinc-900/10 backdrop-opacity-20',
         }}
