dc5873ba5d9e2fe3c7d9bfc6902384ae7ad7cbb5
MichaelMaherKamel <michaelmaherkamel@gmail.com>
2023-10-09 13:51:41 +0300

    Fixed Styles for Dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,8 @@
 import { Metadata } from 'next'
 import Dashboard from '@/components/dashboard/dashboard'
 
+export const dynamic = 'force-dynamic'
+
 export const metadata: Metadata = {
   title: 'Dashboard',
   description: 'User Dashboard to Check Stores, Products and Stats',
