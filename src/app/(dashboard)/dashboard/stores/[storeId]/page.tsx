212945d34aa778392a4d2c143ee05a93b2b0b1da
MichaelMaherKamel <michaelmaherkamel@gmail.com>
2023-10-12 00:32:42 +0300

    Created StoreId Dasboard Page

--- src/app/(dashboard)/dashboard/stores/[storeId]/page.tsx
+++ src/app/(dashboard)/dashboard/stores/[storeId]/page.tsx
@@ -6,13 +6,12 @@ import Dashboard from '@/components/dashboard/dashboard'
 import { serverClient } from '@/lib/trpc/server'
 
 interface StorePageProps {
-  children: React.ReactNode
   params: {
     storeId: string
   }
 }
 
-export default async function StorePage({ children, params }: StorePageProps) {
+export default async function StorePage({ params }: StorePageProps) {
   const StoreID = Number(params.storeId)
   const stores = await serverClient.stores.getStores()
   const store = stores.stores.find((store) => store.id === StoreID)
@@ -24,7 +23,6 @@ export default async function StorePage({ children, params }: StorePageProps) {
       <div className='flex items-center space-x-4'></div>
       <div className='space-y-4 overflow-hidden'>
         <Dashboard storeId={StoreID} />
-        {children}
       </div>
     </Shell>
   )
