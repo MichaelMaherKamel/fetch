0c913bc7db7a3f1506b6868800e71f7b9ad8717c
MichaelMaherKamel <michaelmaherkamel@gmail.com>
2023-11-04 15:48:23 +0200

    Testing EdgeStore

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,8 @@ import { ThemeProvider } from '@/components/ui/theme-provider'
 import TrpcProvider from '@/lib/trpc/Provider'
 import { Toaster } from '@/components/ui/toaster'
 
+import { EdgeStoreProvider } from '@/lib/edgestore'
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -18,12 +20,15 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang='en'>
       <body className={inter.className}>
         <NextAuthProvider>
-          <ThemeProvider attribute='class' defaultTheme='system' enableSystem>
-            <TrpcProvider>
-              {children}
-              <Toaster />
-            </TrpcProvider>
-          </ThemeProvider>
+          <EdgeStoreProvider>
+            {' '}
+            <ThemeProvider attribute='class' defaultTheme='system' enableSystem>
+              <TrpcProvider>
+                {children}
+                <Toaster />
+              </TrpcProvider>
+            </ThemeProvider>
+          </EdgeStoreProvider>
         </NextAuthProvider>
       </body>
     </html>
