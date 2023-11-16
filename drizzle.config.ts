cbe7d81a03eee2a79b9636ab3e61ee14b1beb6a1
MichaelMaherKamel <michaelmaherkamel@gmail.com>
2023-11-16 12:36:52 +0200

    Created Products Table

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,11 @@
-import type { Config } from "drizzle-kit";
-import { env } from "@/lib/env.mjs";
+import type { Config } from 'drizzle-kit'
+import { env } from '@/lib/env.mjs'
 
 export default {
-  schema: "./src/lib/db/schema",
-  out: "./src/lib/db/migrations",
-  driver: "mysql2",
+  schema: './src/lib/db/schema',
+  out: './src/lib/db/migrations',
+  driver: 'mysql2',
   dbCredentials: {
-    connectionString: env.DATABASE_URL,
-  }
-} satisfies Config;
+    connectionString: env.NEXT_PUBLIC_DATABASE_URL,
+  },
+} satisfies Config
